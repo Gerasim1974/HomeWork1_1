835710a320049bb5eb55f26ec46fe6e3f4aef37a
ПК <GIS_1974@tut.by>
2022-07-15 12:36:14 +0300

    HomeWork1 Task1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string? sNumber = Console.ReadLine();
+            int Number = -int.Parse(sNumber);
+            Console.WriteLine(Number);
+            Console.ReadKey();
+        }
+    }
+}
